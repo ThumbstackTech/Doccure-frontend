c374e6cca05dc9eb4e0519a381234fc1b422baab
Akash Dasgupta <bloombergakash@gmail.com>
2021-12-14 14:38:02 +0530

    Multiple Fab

--- src/components/FabButton/fab.js
+++ src/components/FabButton/fab.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './fab.css';
 import $ from 'jquery';
 import { useSelector } from 'react-redux';
-import icon from "../../icons/icons8-circular-arrows-50.png";
+import icon from '../../icons/icons8-circular-arrows-50.png';
 
 // import axios from 'axios';
 
@@ -17,56 +17,70 @@ export const Fab = () => {
   let left = total - current;
   return (
     <>
-      <div id='overlay' onClick={()=>{
+      
+      <div className="outerDiv">
+      <div
+        id='overlay'
+        onClick={() => {
           $('.fab').removeClass('active');
           $('#overlay').removeClass('dark-overlay');
-          $('#loadicon').css('display','block');
-      }}></div>
-      <div class='fab' onClick={()=>{
-          $('.fab').addClass('active');
-          $('#overlay').addClass('dark-overlay');
-          $('#loadicon').css('display','none');
-  }}>
-        <img id="loadicon" style={{position:"absolute"}} src={icon} alt=""/>
-        <h1 style={{fontSize:"20px"}}>7</h1>
-        <div className="drname"><h2>Dr.XYZ</h2></div>
-        <div className='row cntt-wrapper'>
-          <div className='dash-widget-info'>
-            <h6 style={{ fontSize: '1.1rem' }} className='text-danger'>
-              Total Patients In Queue
-            </h6>
-            <div className='dash-widget-count'>
-              <h3>{appointments && appointments.length}</h3>
-            </div>
+          $('#loadicon').css('display', 'block');
+          $(".outerDiv").css('width','auto');
+          $(".outerDiv").css('height','auto');
+          $(".outerDiv").css('top','40%');
+        }}
+      ></div>
+        <div
+          class='fab'
+          onClick={() => {
+            $('.fab').addClass('active');
+            $('#overlay').addClass('dark-overlay');
+            $('#loadicon').css('display', 'none');
+            $(".outerDiv").css('width','100%');
+            $(".outerDiv").css('height','100%');
+            $(".outerDiv").css('top','auto');
+          }}
+        >
+          <img
+            id='loadicon'
+            style={{ position: 'absolute' }}
+            src={icon}
+            alt=''
+          />
+          <h1 style={{ fontSize: '20px' }}>7</h1>
+          <div className='drname'>
+            <h2>Dr.XYZ</h2>
           </div>
-          <div className='dash-widget-info'>
-            <h6 style={{ fontSize: '1.1rem' }} className='text-success'>
-              Current Patient Token No.
-            </h6>
-            <div className='dash-widget-count'>
-              <h3>{consultedappointments && consultedappointments.length}</h3>
+          <div className='row cntt-wrapper'>
+            <div className='dash-widget-info'>
+              <h6 style={{ fontSize: '1.1rem' }} className='text-danger'>
+                Total Patients In Queue
+              </h6>
+              <div className='dash-widget-count'>
+                <h3>{appointments && appointments.length}</h3>
+              </div>
             </div>
-          </div>
-          <div className='dash-widget-info'>
-            <h6 style={{ fontSize: '1.1rem' }} className='text-info'>
-              Patients Left
-            </h6>
-            <div className='dash-widget-count'>
-              <h3>{left}</h3>
+            <div className='dash-widget-info'>
+              <h6 style={{ fontSize: '1.1rem' }} className='text-success'>
+                Current Patient Token No.
+              </h6>
+              <div className='dash-widget-count'>
+                <h3>{consultedappointments && consultedappointments.length}</h3>
+              </div>
+            </div>
+            <div className='dash-widget-info'>
+              <h6 style={{ fontSize: '1.1rem' }} className='text-info'>
+                Patients Left
+              </h6>
+              <div className='dash-widget-count'>
+                <h3>{left}</h3>
+              </div>
             </div>
           </div>
         </div>
+
+        
       </div>
-      {/* <div class="mdl-grid">
-                <div class="mdl-cell mdl-cell--5-col mdl-cell--12-col-tablet mdl-shadow--2dp center m-t-100">
-                    <div class="box">
-                    <h1>Material Popup</h1>
-                    <h4>Just click the fancy button to see the magic</h4>
-                    <br></br>
-                    <p>This example uses the MDL-Libary for the CSS layout,<br/> the popup itself works it just fine without.</p>
-                    </div>
-                </div>
-            </div> */}
     </>
   );
 };
