31ab72df9e44289b73efa3d01edd78475fb9c2f3
AmreeneEbrahim <amreen1294@gmail.com>
2021-12-24 12:36:58 +0530

    api link

--- src/vars/urls/index.js
+++ src/vars/urls/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8000/";
+// export const API_URL = "http://localhost:8000/";
 
-// export const API_URL = "https://doccure-api.herokuapp.com/";
+export const API_URL = "https://doccure-api.herokuapp.com/";
